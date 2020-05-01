10f86be2abd12ec3d7231b0587f49acd58abd1d1
AhmedSeef <ahmed.seef.gooda@gmail.com>
2020-05-01 16:57:03 +0200

    update user register

---
+++ src/app/models/user-add.ts
@@ -0,0 +1,17 @@
+export class UserAdd {
+    username:string;
+    user_type:string;
+    first_name:string;
+    last_name:string;
+    email:string;
+    phone:string;
+    birth_date:Date;
+    adress:string;
+    password:string;
+    stage:Number;
+    category:Number;
+    hire_date:Date;
+    job:string;
+    picture:string;
+    
+}
