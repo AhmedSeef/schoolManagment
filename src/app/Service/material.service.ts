a49257bc2309d7ce22ee708bd38e2fc1a9a05e1f
AhmedSeef <ahmed.seef.gooda@gmail.com>
2020-07-21 13:29:27 +0200

    edit material

--- src/app/Service/material.service.ts
+++ src/app/Service/material.service.ts
@@ -19,7 +19,9 @@ export class MaterialService {
        'Authorization': "token "+ this.token 
     });
     this.headers_object.append('enctype', 'multipart/form-data');
-  
+    this.headers_object.set('Content-Type', 'false');
+    this.headers_object.set('mimeType', 'application/json');
+    
     this.httpOptions = {
       headers: this.headers_object
     };
@@ -29,7 +31,24 @@ export class MaterialService {
     return this.http.get(this.baseUrl,this.httpOptions);
   }
 
-save(material){
-  return this.http.post(this.baseUrl,material,this.httpOptions)
+save(material){ 
+
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Token "+this.token);
+
+
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: material,
+  
+};
+
+return fetch("http://127.0.0.1:8000/api/materials/", requestOptions)
+  .then(response => alert("added succefully"))
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  
 }
 }
--- src/app/modules/pages/add-material/add-material.component.ts
+++ src/app/modules/pages/add-material/add-material.component.ts
@@ -18,7 +18,7 @@ export class AddMaterialComponent implements OnInit {
     notes:""
   };
 
-  formData:FormData = new FormData();
+  form:FormData = new FormData();
   
   constructor(private materialService:MaterialService,private subjectService:SbjectService) { }
 
@@ -32,35 +32,23 @@ export class AddMaterialComponent implements OnInit {
     this.model.subject = subjectId;
   }
 
-  /*changeFile(files: FileList){
-    /*this.model.path = files.item(0);
-    let formData = new FormData();
-    formData.append('path',this.model.path.name);
   
-   let testData:FormData = new FormData();
-   console.log(testData)
-  }*/
 
   fileChange(event) {
     let fileList: FileList = event.target.files;
     if(fileList.length > 0) {
-      let file: File = fileList[0];
-      console.log(file)
+      let file: File = fileList[0];   
      
-      this.formData.append('uploadFile', file, file.name);
-     
-      console.log(this.formData)
-      
-      
+    this.form.append("subject", this.model.subject.toString());
+    this.form.append("material_type", "pdf");
+    this.form.append("title", this.model.title);
+    this.form.append("path", file, file.name);
+    this.form.append("notes", this.model.notes);
   }
 }
 
   addMaterial(){
-    this.formData =  new FormData(); 
     
-    console.log(this.formData)
-    this.materialService.save(this.formData).subscribe(
-      ()=>{}
-    )
+    this.materialService.save(this.form)
   }
 }
